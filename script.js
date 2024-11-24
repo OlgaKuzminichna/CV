8177853291c7f31527090cda464fc85d0880ced3
olga-litvinova <litvinova2404ad@gmail.com>
2024-11-25 00:52:12 +0100

    css ready

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+    const menu = document.querySelector(".dropdown-links"); 
+    const icon = document.querySelector(".icon-toggle");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs
+    .send("test", "template", params)
+    .then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Message sent successfully!");
+    })
+    .catch((err) => console.log(err));
+}
